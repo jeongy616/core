126fcd8a1caf854c21e128aef70702a73596b048
jeongyeonkim <jeongy616@naver.com>
2022-03-12 23:35:51 +0900

    Initial commit

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,32 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig {
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy(){
+//        return new FixDiscountPolicy();
+        return  new RateDiscountPolicy();
+    }
+
+
+    //생성자를 통해서 주입하는 생성자주입
+    //AppConfig는 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결)해준
+    public MemberService memberService(){
+        return new MemberServiceImpl(memberRepository());
+    }
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(),discountPolicy());
+    }
+}
